33e4cd72c9fc9c886e762713909501f6b34dbd6d
Naman Jain <jnaman475@gmail.com>
2024-12-10 14:38:41 +0530

    testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,15 +11,14 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      sss
-      {/* <h1 className="text-3xl mb-4">Welcome to Next.js Modal</h1>
+      <h1 className="text-3xl mb-4">Welcome to Next.js Modal</h1>
       <button
         onClick={toggleModal}
         className="bg-blue-500 text-white p-4 rounded"
       >
         Open Modal
       </button>
-      <Modal isOpen={isModalOpen} onClose={toggleModal} /> */}
+      <Modal isOpen={isModalOpen} onClose={toggleModal} />
       <Chatbot />
     </div>
   );
