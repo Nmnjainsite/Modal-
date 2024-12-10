db77515776d25c29a6a5cd7f00fcc03191e81082
Naman Jain <jnaman475@gmail.com>
2024-12-10 14:33:15 +0530

    added chatbot.bundle.js file

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import Modal from "./components/modal";
+import Chatbot from "./components/chatbot";
 
 const HomePage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -10,14 +11,16 @@ const HomePage = () => {
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
-      <h1 className="text-3xl mb-4">Welcome to Next.js Modal</h1>
+      sss
+      {/* <h1 className="text-3xl mb-4">Welcome to Next.js Modal</h1>
       <button
         onClick={toggleModal}
         className="bg-blue-500 text-white p-4 rounded"
       >
         Open Modal
       </button>
-      <Modal isOpen={isModalOpen} onClose={toggleModal} />
+      <Modal isOpen={isModalOpen} onClose={toggleModal} /> */}
+      <Chatbot />
     </div>
   );
 };
