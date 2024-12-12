16684f4a1d6e7018d0aedca973a24ff9df8ef19a
Naman Jain <jnaman475@gmail.com>
2024-12-12 16:18:32 +0530

    removed output export from config

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,6 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "export",
   webpack(config) {
     config.resolve.alias["react"] = path.resolve("./node_modules/react");
     config.resolve.alias["react-dom"] = path.resolve(
