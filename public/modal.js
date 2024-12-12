1e01758ff12f0c941cc5eeaff8dd39211b14d140
Naman Jain <jnaman475@gmail.com>
2024-12-12 14:25:38 +0530

    added modal url in iframe

--- public/modal.js
+++ public/modal.js
@@ -1,6 +1,6 @@
 (function () {
   // Define the URL of the iframe content
-  const iframeUrl = "https://trainkore.com";
+  const iframeUrl = "https://modal-rho-six.vercel.app/";
 
   // Create the iframe element
   const iframe = document.createElement("iframe");
@@ -8,13 +8,14 @@
   // Set iframe attributes
   iframe.src = iframeUrl;
   iframe.style.cssText = `
-    width: 100%;
-    height: 600px;
+    width: 400px; /* Fixed width */
+    height: 600px; /* Fixed height */
     border: none;
     position: fixed;
     bottom: 0;
-    left: 0;
-    z-index: 9999;
+    right: 0; /* Align to the bottom-right corner */
+    z-index: 99999;
+    pointer-events: auto;
   `;
 
   // Append the iframe to the body
