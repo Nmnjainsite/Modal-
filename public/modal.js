0e45403e203460fd7ea199bb551099e92a3a1c30
Naman Jain <jnaman475@gmail.com>
2024-12-13 13:28:45 +0530

    removed shadow

--- public/modal.js
+++ public/modal.js
@@ -9,15 +9,13 @@
   iframe.src = iframeUrl;
   iframe.style.cssText = `
     width: 400px; /* Fixed width */
-    height: auto;
-    max-height: 70vh; /* To limit the height if necessary */
+    height:600px; /* Fixed height */
     border: none;
     position: fixed;
-    bottom: 10px; /* Fixed at the bottom */
-    right: 10px; /* Fixed on the right side */
-    z-index: 99999; /* Ensure it appears above other elements */
-    box-shadow: 0px 4px 12px rgba(0, 0, 0, 0.1);
-    border-radius: 10px; /* Smooth rounded edges */
+    bottom: 0; /* 10px space from the bottom */
+    right: 10px; /* Align to the bottom-right corner */
+    z-index: 99999;
+    pointer-events: auto;
   `;
 
   // Append the iframe to the body
