ec2b90286fd47f0b9b6dda5992055eeee40da846
Naman Jain <jnaman475@gmail.com>
2025-03-12 15:32:15 +0530

    added script tag in modal.js file

--- public/modal.js
+++ public/modal.js
@@ -1,6 +1,19 @@
 (function () {
-  // Define the URL of the iframe content
-  const iframeUrl = "https://modal-rho-six.vercel.app/";
+  // Function to get the userId from the script URL
+  function getQueryParam(param) {
+    const scripts = document.getElementsByTagName("script");
+    const currentScript = scripts[scripts.length - 1]; // Get the last loaded script (i.e., this one)
+    const scriptUrl = new URL(currentScript.src); // Get the script URL
+    return scriptUrl.searchParams.get(param); // Get the userId from the script URL
+  }
+
+  // Get the userId from the script tag URL
+  const userId = getQueryParam("userId");
+
+  // Define the URL of the iframe content, appending the userId as a query parameter
+  const iframeUrl = `https://modal-rho-six.vercel.app/?userId=${encodeURIComponent(
+    userId
+  )}`;
 
   // Create the iframe element
   const iframe = document.createElement("iframe");
@@ -8,15 +21,14 @@
   // Set iframe attributes
   iframe.src = iframeUrl;
   iframe.style.cssText = `
-      width: 400px; /* Fixed width */
-      height: 550px; /* Fixed height */
-      border: none;
-      position: fixed;
-      bottom: 0; /* 10px space from the bottom */
-      right: 10px; /* Align to the bottom-right corner */
-      z-index: 99999;
-      pointer-events: auto;
-     
+    width: 400px; /* Fixed width */
+    height: 550px; /* Fixed height */
+    border: none;
+    position: fixed;
+    bottom: 0; /* 10px space from the bottom */
+    right: 10px; /* Align to the bottom-right corner */
+    z-index: 99999;
+    pointer-events: auto;
   `;
 
   // Append the iframe to the body
